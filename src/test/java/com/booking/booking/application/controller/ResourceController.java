8e9b6cc3f338e7d0bf30c4dd7195277a7acad4d5
DiegoKrumdieck <wfcchamp123@gmail.com>
2023-12-24 11:46:11 -0500

    first commit

---
+++ src/test/java/com/booking/booking/application/controller/ResourceController.java
@@ -0,0 +1,18 @@
+package com.booking.booking.application.controller;
+
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/resources")
+public class ResourceController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Hello world!";
+    }
+}
